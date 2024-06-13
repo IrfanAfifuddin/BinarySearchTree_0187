7910d474ff1b9a16ed1e64f242cdd92f453d1b47
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-06-13 10:54:48 +0700

    membuat binarytree

--- BinarySearchTree_0187/BinarySearchTree_0187.cpp
+++ BinarySearchTree_0187/BinarySearchTree_0187.cpp
@@ -7,6 +7,18 @@ public:
     string info;
     node* leftchild;
     node* rightchild;
+
+    // Constructor for the node class
+    node(string i, node* l, node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
+class BinaryTree {
+
 };
 
 int main()
