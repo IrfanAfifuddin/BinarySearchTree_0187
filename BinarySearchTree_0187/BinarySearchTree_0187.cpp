855ab1e64c21b7dd8ad06baae61712001ac41d0b
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-06-13 11:13:59 +0700

    membuat kondisi else if

--- BinarySearchTree_0187/BinarySearchTree_0187.cpp
+++ BinarySearchTree_0187/BinarySearchTree_0187.cpp
@@ -47,6 +47,11 @@ public:
         {
             parent->leftchild = newNode; // Make the left child of the parent to the new node
         }
+
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
     }
 };
 
