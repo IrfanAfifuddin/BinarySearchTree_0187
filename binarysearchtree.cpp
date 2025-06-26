da5f7f0369e72b4f8580f71957aa90e0fcb472a2
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-06-26 18:23:16 +0700

    membuat node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -7,4 +7,11 @@ public:
     int info;
     Node *leftchild;
     Node *rightchild;
+
+    // constructor for node class
+    Node()
+    {
+        leftchild = nullptr;  // initialize left child to null
+        rightchild = nullptr; // initialize right child to null
+    }
 };
